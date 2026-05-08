43af3d61cfa0a03f9e9bc4824e5774e8cc8b688f
Abdurahmon Sultonov <sultonovabdurahmon2005@gmail.com>
2026-05-08 12:58:01 +0500

    JS Lessons

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ console.log("\x1b[31m%s\x1b[0m", "This is red text!");
 console.log("Мой возраст:", 2026 - 2005, 'лет');
 
 // Написать первый alert() — браузер показывает диалог. Сравнить с console.log.
-alert("Добро пожаловать!")
+//alert("Добро пожаловать!")
 
 
 //Vazifa
@@ -35,5 +35,47 @@ console.log(21 * 365)
 
 
 //Приветствие и вопрос
-alert("Привет! Добро пожаловать в JavaScript!");
-let isReady = confirm("Готов учиться?");
+//0alert("Привет! Добро пожаловать в JavaScript!");
+//let isReady = confirm("Готов учиться?");
+
+
+
+// 1. Создание переменных
+const name = "Abdurahmon";
+let age = 21;
+const city = "Khujand";
+let isOnline = true;
+
+
+// 2. Вывод переменных отдельно
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(isOnline);
+
+
+// Проверка типов
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof isOnline);
+
+
+// 3. Красивый вывод через шаблонную строку
+console.log(`Имя: ${name}, Возраст: ${age}, Город: ${city}`);
+
+
+// 4. Изменение let переменных
+age = 21;
+isOnline = false;
+
+console.log(age);
+console.log(isOnline);
+
+
+// Попытка изменить const переменную
+// name = "Ali"; // Ошибка! const нельзя изменять
+
+
+// 5. Вычисление года рождения
+console.log(`Год рождения: ${new Date().getFullYear() - age}`);
